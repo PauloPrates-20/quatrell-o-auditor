54f2c57fe8ee20695c0724543c4042e8133efcb0
Paulo Prates <paulo.prates01@hotmail.com>
2025-04-10 09:04:38 -0300

    refactor: updated config.ts to find the correct env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 // dotenv configuration
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+const env = process.env.NODE_ENV || 'development'
+const envPath = path.resolve(process.cwd(), `.env.${env}`);
+
+dotenv.config({ path: envPath });
 
 const {
   DISCORD_TOKEN,
