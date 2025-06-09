3f67012a185c33d0b420f346cfb7de970aed709b
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-09 16:46:11 -0300

    refactor: updated Player class

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -5,96 +5,40 @@ export class Player {
   id: string;
   gold: number;
   gems: Gems;
-  characters: CharacterDef;
+  characters: Character[];
 
-	constructor(id: string, gold = 0, gems = { comum: 0, transmutacao: 0, ressureicao: 0 }, characters = {}) {
+	constructor(id: string, gold = 0, gems = { comum: 0, transmutacao: 0, ressureicao: 0 }, characters = []) {
 		this.id = id;
 		this.gold = gold;
 		this.gems = gems;
 		this.characters = characters;
 	}
 
-	/* Mutation methods */
+	// Adds or subtracts gold to the player
+	updateGold(amount: number, set?: boolean) {
+    if (set) this.gold = amount;
+    else this.gold += amount; 
+  }
 
-	// Adds gold to the player
-	addGold(amount: number) {
-		this.gold += amount;
-	}
-
-	// Removes gold from the player
-	subGold(amount: number) {
-		this.gold -= amount;
+  // Adds or subtracts gems to the player
+	updateGems(type: keyof Gems, amount: number, set?: boolean) {
+    if (set) this.gems[type] = amount;
+		else this.gems[type] += amount;
 	}
 
 	// Adds a character to the player
-	registerCharacter(character: Character, characterKey: string) {
-		this.characters[characterKey] = Object.assign({}, character);
-	}
+	registerCharacter(character: Character) { this.characters.push({ ...character }); }
+
+  getCharacterIndex(name: string) { return this.characters.findIndex(character => character.name === name); }
+
+  getCharacter(name: string) {
+    let character = this.characters[this.getCharacterIndex(name)];
+
+    return character;
+  }
 
 	// Deletes a character from the player
-	deleteCharacter(characterKey: string) {
-		delete this.characters[characterKey];
-	}
-
-	// Renames a character (Create a new character with the same stats and delete the old)
-	renameCharacter(oldKey: string, newKey: string, newName: string) {
-		this.characters[newKey] = { ...this.characters[oldKey], name: newName };
-		delete this.characters[oldKey];
-	}
-
-	// Add xp to a character
-	addXp(characterKey: string, amount: number) {
-		const character = this.characters[characterKey];
-		character.xp += amount;
-
-		this.changeLevel(character);
-	}
-
-	// Subtract xp from a character
-	subXp(characterKey: string, amount: number) {
-		const character = this.characters[characterKey];
-		character.xp -= amount;
-
-		this.changeLevel(character);
-	}
-
-	// Sets the character xp value to the amount parsed
-	setXp(characterKey: string, amount: number) {
-		const character = this.characters[characterKey];
-		character.xp = amount;
-
-		this.changeLevel(character);
-	}
-
-	// Changes character level
-	changeLevel(character: Character) {
-		for (const level of levelsTable) {
-			if (character.xp >= level.xp) {
-				character.level = level.level;
-			}
-		}
-
-		this.changeTier(character);
-	}
-
-	// Changes character tier
-	changeTier(character: Character) {
-		for (const tier of tiersTable) {
-			if (character.level >= tier.level) {
-				character.tier = tier.tier;
-			}
-		}
-	}
-
-	// Adds gems to the player
-	addGems(type: keyof Gems, amount: number) {
-		this.gems[type] += amount;
-	}
-
-	// Removes gems from the player
-	subGems(type: keyof Gems, amount: number) {
-		this.gems[type] -= amount;
-	}
+	deleteCharacter(name: string) { this.characters.splice(this.getCharacterIndex(name)); }
 }
 
 export class Character {
