e333ae45ce936a29dc441233774dce7e970859c6
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-14 14:24:05 -0300

    refactor: updated the cliente command `/listar` to the new class structure

--- src/commands/client/listar.ts
+++ src/commands/client/listar.ts
@@ -1,6 +1,7 @@
 /* Imports */
 import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js';
 import { loadPlayer } from '../../lib/firebase/firestoreQuerys';
+import { playerValidation } from '../../lib/validation';
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,23 +13,19 @@ module.exports = {
     const author = interaction.user.id;
 		const player = await loadPlayer(author);
 
-		if (!player) {
-			await interaction.editReply('Jogador não cadastrado. Utilize o comando `/registrar` para se cadastrar.');
+    let validation = playerValidation(player);
+		if (typeof validation === 'string') {
+			await interaction.editReply(validation);
 			return;
 		}
 
-		const characters = player.characters;
-
-		if (Object.keys(characters).length === 0) {
+		if (player!.characters.length === 0) {
 			await interaction.editReply('Nenhum personagem cadastrado. Utilize o comando `/personagem` para começar a cadastrar seus personagens.');
 			return;
 		}
 
 		let text = '';
-		for (const name in characters) {
-			const character = characters[name];
-			text += `Nome: ${character.name}\nXP: ${character.xp}\nNível: ${character.level}\nTier: ${character.tier}\n\n`;
-		}
+		for (const character of player!.characters) text += `Nome: ${character.name}\nXP: ${character.xp}\nNível: ${character.level}\nTier: ${character.tier}\n\n`;
 
 		await interaction.editReply(`Lista de personagens:\n\n${text}`);
 	},
