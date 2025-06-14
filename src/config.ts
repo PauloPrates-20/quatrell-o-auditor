3239daed8b457dbc45bbed29be41bb6f59ed47f3
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-14 12:47:09 -0300

    refactor: asserted non null key values

--- src/config.ts
+++ src/config.ts
@@ -36,28 +36,28 @@ const {
 
 // Firebase configs
 export const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
+  apiKey: FIREBASE_API_KEY!,
+  authDomain: FIREBASE_AUTH_DOMAIN!,
+  projectId: FIREBASE_PROJECT_ID!,
+  storageBucket: FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID!,
+  appId: FIREBASE_APP_ID!,
 };
 export const collections = {
-  users: COLLECTIONS_USERS,
+  users: COLLECTIONS_USERS!,
 };
 
 // Discord configs
-export const token = DISCORD_TOKEN;
-export const clientId= DISCORD_CLIENT_ID;
-export const guildId = DISCORD_GUILD_ID;
+export const token = DISCORD_TOKEN!;
+export const clientId= DISCORD_CLIENT_ID!;
+export const guildId = DISCORD_GUILD_ID!;
 
 // Channel IDs
 export const channels = {
-  bank: CHANNELS_BANK,
-  xp: CHANNELS_XP,
-  treasure: CHANNELS_TREASURE,
-  transferencies: CHANNELS_TRANSFERENCIES,
-  general: CHANNELS_GENERAL,
-  shop: CHANNELS_SHOP,
+  bank: CHANNELS_BANK!,
+  xp: CHANNELS_XP!,
+  treasure: CHANNELS_TREASURE!,
+  transferencies: CHANNELS_TRANSFERENCIES!,
+  general: CHANNELS_GENERAL!,
+  shop: CHANNELS_SHOP!,
 };
