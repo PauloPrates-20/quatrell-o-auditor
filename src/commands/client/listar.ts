8c0a3e0adf3f9184ef8ecc973ac919e951b7265b
Paulo Prates <paulo.prates01@hotmail.com>
2026-02-27 23:41:36 -0300

    refactor command /listar

--- src/commands/client/listar.ts
+++ src/commands/client/listar.ts
@@ -1,35 +1,37 @@
 /* Imports */
 import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder } from 'discord.js';
 import { loadPlayer } from '../../lib/firebase/firestoreQuerys';
+import { CharacterDef } from '../../lib/definitions';
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('listar')
-		.setDescription('Exibe uma lista de seus personagens cadastrados.'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		await interaction.deferReply({ flags: 'Ephemeral' });
-    
-    const author = interaction.user.id;
-		const player = await loadPlayer(author);
+    data: new SlashCommandBuilder()
+        .setName('listar')
+        .setDescription('Exibe uma lista de seus personagens cadastrados.'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply({ flags: 'Ephemeral' });
 
-		if (!player) {
-			await interaction.editReply('Jogador não cadastrado. Utilize o comando `/registrar` para se cadastrar.');
-			return;
-		}
+        const author = interaction.user.id;
+        let player;
 
-		const characters = player.characters;
+        try {
+            player = await loadPlayer(author);
+        } catch(e: any) {
+            await interaction.editReply(e.message);
+            return;
+        }
 
-		if (Object.keys(characters).length === 0) {
-			await interaction.editReply('Nenhum personagem cadastrado. Utilize o comando `/personagem` para começar a cadastrar seus personagens.');
-			return;
-		}
+        const characters = player.characters;
 
-		let text = '';
-		for (const name in characters) {
-			const character = characters[name];
-			text += `Nome: ${character.name}\nXP: ${character.xp}\nNível: ${character.level}\nTier: ${character.tier}\n\n`;
-		}
+        if (Object.keys(characters).length === 0) {
+            await interaction.editReply('Nenhum personagem cadastrado. Utilize o comando `/personagem` para começar a cadastrar seus personagens.');
+            return;
+        }
 
-		await interaction.editReply(`Lista de personagens:\n\n${text}`);
-	},
+        let text = '';
+        for (const char of characters) {
+            text += `Nome: ${char.name}\nXP: ${char.xp}\nNível: ${char.level}\nTier: ${char.tier}\n\n`;
+        }
+
+        await interaction.editReply(`Lista de personagens:\n\n${text}`);
+    },
 };
