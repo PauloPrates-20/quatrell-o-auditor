4d08e21f43809e9a236bb5cbf648727389a0ce41
Paulo Prates <paulo.prates01@hotmail.com>
2026-02-27 23:57:48 -0300

    refactor command /registrar

--- src/commands/client/registrar.ts
+++ src/commands/client/registrar.ts
@@ -4,28 +4,31 @@ import { loadPlayer, registerPlayer } from '../../lib/firebase/firestoreQuerys';
 import { Player } from '../../lib/classes';
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('registrar')
-		.setDescription('Registra o jogador no banco de dados do servidor.'),
-	async execute(interaction: ChatInputCommandInteraction) {
-    const author = interaction.user.id;
-		await interaction.deferReply({ flags: 'Ephemeral' });
-		let player = await loadPlayer(author);
+    data: new SlashCommandBuilder()
+        .setName('registrar')
+        .setDescription('Registra o jogador no banco de dados do servidor.'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const author = interaction.user.id;
+        await interaction.deferReply({ flags: 'Ephemeral' });
+        let player;
 
-		if (player) {
-			await interaction.editReply('Jogador já cadastrado!');
-			return;
-		}
+        try {
+            player = await loadPlayer(author);
+        } catch (e) { }
 
-		player = new Player(author);
+        if (player) {
+            await interaction.editReply('Jogador já cadastrado!');
+            return;
+        }
 
-		try {
-			await registerPlayer(player);
+        player = new Player({ id: author });
 
-			await interaction.editReply('Jogador cadastrado com sucesso!');
-			return;
-		} catch (error) {
-			await interaction.editReply('Falha ao cadastrar jogador: ' + error);
-		}
-	},
+        try {
+            await registerPlayer(player);
+
+            await interaction.editReply('Jogador cadastrado com sucesso!');
+        } catch (e: any) {
+            await interaction.editReply('Falha ao cadastrar jogador: ' + e.message);
+        }
+    },
 };
