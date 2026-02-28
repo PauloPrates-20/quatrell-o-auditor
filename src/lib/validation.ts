129f2ea8042188ea4b43675ea3149f939f61f9b6
Paulo Prates <paulo.prates01@hotmail.com>
2026-02-27 23:29:19 -0300

    refactor command /depositar

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,7 +1,9 @@
 // Validates the source for the transactions within the bot.
 // Source must be an url pointing to a discord message
-export function sourceValidation(source: string): boolean {
+export function validateSource(source: string) {
 	const pattern = /^https:\/\/discord.com\/channels\/\d{18,}\/\d{18,}\/\d{18,}$/m;
 
-	return pattern.test(source);
+    if(!pattern.test(source)) {
+        throw new Error('Origem inválida.');
+    }
 }
