645095ea9d2e8d73bfad4adae93392e18964aad0
Paulo Prates <paulo.prates01@hotmail.com>
2026-03-01 15:41:48 -0300

    created /bau command

---
+++ src/commands/client/bau.ts
@@ -0,0 +1,148 @@
+import { TextChannel, ChatInputCommandInteraction, SlashCommandBuilder, InteractionResponse } from 'discord.js';
+import { loadPlayer, updatePlayer, registerLog } from '../../lib/firebase/firestoreQuerys';
+import { channels } from '../../config';
+import { Character, Log, Player } from '../../lib/classes';
+import { Item } from '../../lib/definitions';
+import { inventoryLogBuilder } from '../../lib/messages';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bau')
+        .setDescription('Gerencia o inventário de um personagem.')
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName('depositar')
+                .setDescription('Adiciona um item ao inventário do personagem')
+                .addStringOption(option =>
+                    option
+                        .setName('personagem')
+                        .setDescription('Nome do personagem')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('item')
+                        .setDescription('Item a adicionar')
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option
+                        .setName('quantidade')
+                        .setDescription('Quantidade de items para adicionar.')
+                        .setRequired(true)
+                        .setMinValue(1)
+                )
+                .addIntegerOption(option =>
+                    option
+                        .setName('preço')
+                        .setDescription('Valor do item')
+                        .setRequired(true)
+                        .setMinValue(1)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('origem')
+                        .setDescription('Link da mensagem ou motivo do depósito.')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName('retirar')
+                .setDescription('Retira um item do inventário do personagem')
+                .addStringOption(option =>
+                    option
+                        .setName('personagem')
+                        .setDescription('Nome do personagem')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('item')
+                        .setDescription('Item a retirar')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                )
+                .addIntegerOption(option =>
+                    option
+                        .setName('quantidade')
+                        .setDescription('Quantidade de items para retirar.')
+                        .setMinValue(1)
+                        .setRequired(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('origem')
+                        .setDescription('Link da mensagem ou motivo da retirada.')
+                        .setRequired(true)
+                )
+        ),
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply({ flags: 'Ephemeral' });
+
+        const author = interaction.user!.id;
+        let player: Player;
+        let character: Character;
+        const charName = interaction.options.getString('personagem')!;
+        const itemName = interaction.options.getString('item')!;
+        const amount = interaction.options.getInteger('quantidade')!;
+        const source = interaction.options.getString('origem')!;
+        const subcommand = interaction.options.getSubcommand();
+        const inventoryChannel = interaction.client.channels.cache.get(channels.inventory!) as TextChannel;
+
+        try {
+            player = await loadPlayer(author);
+            character = new Character({ ...player.getCharacter(charName) });
+        } catch(e: any) {
+            await interaction.editReply(e.message);
+            return;
+        }
+
+        if(subcommand === 'depositar') {
+            const price = interaction.options.getInteger('preço')!;
+            const item: Item = {
+                name: itemName,
+                price,
+                count: amount,
+            }
+
+            try {
+                character.addItem(item);
+                player.updateCharacter(charName, character);
+                
+                const log = new Log('item', author, inventoryChannel.id, inventoryLogBuilder(author, 'deposita', character, item, source));
+
+                await Promise.all([
+                    updatePlayer(player),
+                    registerLog(log, author),
+                    inventoryChannel.send(log.content),
+                ]);
+
+                await interaction.editReply(`${amount}x ${itemName} depositado(s) com sucesso.`);
+            } catch(e: any) {
+                await interaction.editReply(`Falha ao depositar itens: ${e.message}`);
+            }
+        } else if(subcommand === 'retirar') {
+            try {
+                const item = { ...character.getItem(itemName), count: amount };
+
+                character.removeItem(item);
+                player.updateCharacter(charName, character);
+
+                const log = new Log('item', author, inventoryChannel.id, inventoryLogBuilder(author, 'retira', character, item, source));
+
+                await Promise.all([
+                    updatePlayer(player),
+                    registerLog(log, author),
+                    inventoryChannel.send(log.content),
+                ]);
+
+                await interaction.editReply(`${amount}x ${itemName} retirado(s) com sucesso.`);
+            } catch(e: any) {
+                await interaction.editReply(`Falha ao retirar itens: ${e.message}`);
+            }
+        }
+    }
+}
