c9ef7debd44b6b8e2c8eb3f164a097ae9d82a82f
Paulo Prates <paulo.prates01@hotmail.com>
2026-03-07 13:23:46 -0300

    V4.0.0: Updated API and database

--- src/router.ts
+++ src/router.ts
@@ -1,77 +1,73 @@
 import { Request, Response, Router } from 'express';
 import { client } from './main';
-import { Log } from './lib/classes';
+import { Character, Log } from './lib/classes';
 import { loadPlayer, updatePlayer, registerLog } from './lib/firebase/firestoreQuerys';
 import { channels } from './config';
-import { goldLogBuilder, purchaseLogBuilder } from './lib/messages';
+import { goldLogBuilder, inventoryLogBuilder, purchaseLogBuilder } from './lib/messages';
 import { TextChannel } from 'discord.js';
+import { Item } from './lib/definitions';
 
 const router = Router();
 
 export default router
-  .post('/buy', async (req: Request, res: Response) => {
-    const { accessToken, item, characterKey } = req.body;
+    .post('/buy', async (req: Request, res: Response) => {
+        const { accessToken, item, charName } = req.body;
 
-    if (!(accessToken || item || characterKey)) {
-      res.status(400).json({ error: 'Missing required fields!' });
-      return;
-    }
+        if (!(accessToken || item || charName)) {
+            res.status(400).json({ error: 'Missing required fields!' });
+            return;
+        }
 
-    const response = await fetch('https://discord.com/api/oauth2/@me', {
-      headers: { 'Authorization': `Bearer ${req.body.accessToken}` }
-    });
-    const data = await response.json();
+        const response = await fetch('https://discord.com/api/oauth2/@me', {
+            headers: { 'Authorization': `Bearer ${req.body.accessToken}` }
+        });
+        const data = await response.json();
 
-    if (data.code == 0) {
-      res.status(400).json({ ok: false, message: data.message });
-      return;
-    }
+        if (data.code == 0) {
+            res.status(400).json({ ok: false, message: data.message });
+            return;
+        }
 
-    const playerId = data.user.id;
-    const user = await client.users.fetch(playerId);
+        const playerId = data.user.id;
+        const user = await client.users.fetch(playerId);
 
-    if (!user) {
-      res.status(404).json({ error: 'Player not found!' });
-      return;
-    }
+        if (!user) {
+            res.status(404).json({ error: 'Player not found!' });
+            return;
+        }
 
-    const player = await loadPlayer(playerId);
+        try {
+            const player = await loadPlayer(playerId);
+            console.log(charName);
+            const character = new Character({ ...player.getCharacter(charName) });
+            const purchaseChannel = client.channels.cache.get(channels.shop!) as TextChannel;
+            const bankChannel = client.channels.cache.get(channels.bank!) as TextChannel;
+            const inventoryChannel = client.channels.cache.get(channels.inventory!) as TextChannel;
+            const inventoryItem: Item = {
+                name: (item.name as string),
+                count: 1,
+                price: (item.value as number),
+                attuned: false,
+            };
 
-    if (!player) {
-      res.status(404).json({ error: 'Jogador não encontrado! Utilize o comando `/registrar` para se cadastrar.' });
-      return;
-    }
+            player.subGold(inventoryItem.price);
+            character.addItem(inventoryItem);
 
-    const character = player.characters[characterKey];
+            const purchaseLog = new Log('purchase', playerId, purchaseChannel?.id, purchaseLogBuilder(playerId, character, item.name, 1, item.value));
+            const purchaseMessage = await purchaseChannel.send(purchaseLog.content);
+            const goldLog = new Log('gold', playerId, bankChannel.id, goldLogBuilder(player, 'retira', inventoryItem.price, purchaseMessage.url));
+            const inventoryLog = new Log('item', playerId, inventoryChannel.id, inventoryLogBuilder(playerId, 'deposita', character, inventoryItem, purchaseMessage.url));
 
-    if (!character) {
-      res.status(404).json({ error: 'Personagem não encontrado! Utilize o comando `/personagem adicionar` para criar o personagem.'} );
-      return;
-    }
+            await Promise.all([
+                updatePlayer(player),
+                registerLog(goldLog, playerId),
+                registerLog(purchaseLog, playerId),
+                bankChannel.send(goldLog.content),
+                inventoryChannel.send(inventoryLog.content),
+            ]);
 
-    if (player.gold < item.price) {
-      res.status(400).json({ error: 'Ouro insuficiente!' });
-      return;
-    }
-
-    try {
-      const purchaseChannel = client.channels.cache.get(channels.shop!) as TextChannel;
-      const bankChannel = client.channels.cache.get(channels.bank!) as TextChannel;
-      const purchaseLog = new Log('purchase', playerId, purchaseChannel?.id, purchaseLogBuilder(playerId, character, item.name, 1, item.value));
-      const purchaseMessage = await purchaseChannel.send(purchaseLog.content);
-
-      player.subGold(item.value);
-      const goldLog = new Log('gold', playerId, bankChannel.id, goldLogBuilder(player, 'retira', item.value, purchaseMessage.url));
-
-      await Promise.all([
-        bankChannel.send(goldLog.content),
-        updatePlayer(player),
-        registerLog(goldLog, playerId),
-        registerLog(purchaseLog, playerId)
-      ]);
-
-      res.status(200).json({ success: true, message: `Compra realizada com sucesso!` });
-    } catch (error) {
-      res.status(500).json({ error: 'Não foi possível concluir a compra!', details: error });
-    }
-  })
+            res.status(200).json({ success: true, message: `Compra realizada com sucesso!` });
+        } catch (error) {
+            res.status(500).json({ error: 'Não foi possível concluir a compra!', details: error instanceof Error ? error.message : error });
+        }
+    })
